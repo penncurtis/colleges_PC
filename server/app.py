5497bed471471f0d5873aa88a50664d89c86bcce
Penn Curtis <penn.s.m.c@gmail.com>
2023-07-06 12:29:45 -0400

    whole lotta stuff

--- server/app.py
+++ server/app.py
@@ -2,16 +2,16 @@
 
 import ipdb
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
-from models import db, Hotel, Customer, Review
+from models import db, University, Thread, User, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotels.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///colleges.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -19,286 +19,174 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# CORS(app)
+CORS(app)
 
 api = Api(app)
 
-class Hotels(Resource):
+class Universities(Resource):
 
     def get(self):
-        hotels = Hotel.query.all()
-
-        response_body = []
-
-        for hotel in hotels:
-            response_body.append(hotel.to_dict())
-
-        return make_response(jsonify(response_body), 200)
-
+        universities = [university.to_dict() for university in University.query.all()]
+        return make_response(jsonify(universities), 200)
+    
     def post(self):
-        try:
-            new_hotel = Hotel(name=request.get_json().get('name'), image=request.get_json().get('image'))
-            db.session.add(new_hotel)
-            db.session.commit()
+        data = request.get_json()
+        university = University(university_name=data['university_name'])
+        db.session.add(university)
+        db.session.commit()
+        return jsonify({'message': 'University created!'}, 201)
 
-            response_body = new_hotel.to_dict()
-            
-            return make_response(jsonify(response_body), 201)
-        except ValueError as error:
-            response_body = {
-                "error": error.args
-            }
-            return make_response(jsonify(response_body), 422)
+api.add_resource(Universities, "/universities")
 
-
-api.add_resource(Hotels, '/hotels')
-
-class HotelById(Resource):
+class UniversityByID(Resource):
 
     def get(self, id):
-        hotel = Hotel.query.filter(Hotel.id == id).first()
+        university = University.query.filter_by(id=id).first()
+        return make_response(jsonify(university.to_dict()), 200)
 
-        if not hotel:
-            response_body = {
-                "error": "Hotel not found"
-            }
-            status = 404
+api.add_resource(UniversityByID, "/universities/<int:id>")
 
-        else:
-            response_body = hotel.to_dict()
-            customer_list = []
-            for customer in list(set(hotel.customers)):
-                customer_list.append({
-                    "id": customer.id,
-                    "first_name": customer.first_name,
-                    "last_name": customer.last_name
-                })
-            response_body.update({"customers": customer_list})
-            status = 200
-
-        return make_response(jsonify(response_body), status)
-    
-    def patch(self, id):
-        hotel = Hotel.query.filter(Hotel.id == id).first()
-
-        if not hotel:
-            response_body = {
-                "error": "Hotel not found"
-            }
-            return make_response(jsonify(response_body), 404)
-
-        else:
-            try:
-                json_data = request.get_json()
-                for key in json_data:
-                    setattr(hotel, key, json_data.get(key))
-                db.session.commit()
-
-                response_body = hotel.to_dict()
-                return make_response(jsonify(response_body), 200)
-            
-            except ValueError as error:
-                
-                response_body = {
-                    "error": error.args
-                }
-                
-                return make_response(jsonify(response_body), 422)
-    
-    def delete(self, id):
-        hotel = Hotel.query.filter(Hotel.id == id).first()
-
-        if not hotel:
-            response_body = {
-                "error": "Hotel not found"
-            }
-            status = 404
-
-        else:
-            db.session.delete(hotel)
-            db.session.commit()
-
-            response_body = {}
-            status = 204
-
-        return make_response(jsonify(response_body), status)
-
-
-api.add_resource(HotelById, '/hotels/<int:id>')
-
-class Customers(Resource):
-
+class Threads(Resource):
     def get(self):
-        customers = Customer.query.all()
-
-        response_body = []
-        for customer in customers:
-            response_body.append(customer.to_dict())
-        
-        return make_response(jsonify(response_body), 200)
+        threads = [thread.to_dict() for thread in Thread.query.all()]
+        return make_response(jsonify(threads), 200)
     
     def post(self):
-        try:
-            new_customer = Customer(first_name=request.get_json().get('first_name'), last_name=request.get_json().get('last_name'))
+        data = request.get_json()
+        university = University.query.filter_by(id=data['thread_university_id']).first().to_dict()
 
-            db.session.add(new_customer)
-            db.session.commit()
-            
-            return make_response(jsonify(new_customer.to_dict()), 201)
-        except ValueError as error:
-            response_body = {
-                "error": error.args
-            }
-            return make_response(jsonify(response_body), 422)
-
-api.add_resource(Customers, '/customers')
-
-class CustomerById(Resource):
-
-    def get(self, id):
-        customer = Customer.query.filter(Customer.id == id).first()
-
-        if not customer:
-            response_body = {
-                "error": "Customer not found"
-            }
-            status = 404
-        else:
-            response_body = customer.to_dict()
-            status = 200
-
-        return make_response(jsonify(response_body), status)
+        new_thread = Thread(
+            thread_university_id=university['id']
+        )
+        db.session.add(new_thread)
+        db.session.commit()
+        return jsonify({'message': 'Thread created!'}, 201)
     
+api.add_resource(Threads, "/threads")
+
+class ThreadsByUniversity(Resource):
+    def get(self, schoolname):
+        
+        university = University.query.filter_by(university_name=schoolname).first()
+        if not university:
+            return {'errors': "university not found"}, 404
+        threads = [thread.to_dict() for thread in university.threads]
+        # ipdb.set_trace()
+        return make_response(jsonify(threads), 200)
+
+    def post(self):
+        data = request.get_json()
+        university = University.query.filter_by(id=data['thread_university_id']).first().to_dict()
+
+        new_thread = Thread(
+            thread_title=data['thread_title'],
+            thread_university_id=university['id']
+        )
+        db.session.add(new_thread)
+        db.session.commit()
+        return jsonify({'message': 'Thread created!'}, 201)
+
     def patch(self, id):
-        customer = Customer.query.filter(Customer.id == id).first()
+        thread = Thread.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr(thread, attr, data[attr])
+        db.session.commit()
+        response_body = thread.to_dict()
+        return make_response(jsonify(response_body), 202)
 
-        if not customer:
-            response_body = {
-                "error": "Customer not found"
-            }
-            return make_response(jsonify(response_body), 404)
-        else:
-            try:
-                json_data = request.get_json()
-                
-                for key in json_data:
-                    setattr(customer, key, json_data.get(key))
-
-                db.session.commit()
-
-                response_body = customer.to_dict()
-
-                return make_response(jsonify(response_body), 200)
-            except ValueError as error:
-                response_body = {
-                    "error": error.args
-                }
-                return make_response(jsonify(response_body), 422)
-    
     def delete(self, id):
-        customer = Customer.query.filter(Customer.id == id).first()
-        
-        if not customer:
+        thread = Thread.query.filter_by(id=id).first()
+        if not thread:
+            return jsonify({'message': 'Thread not found'}), 404
+        db.session.delete(thread)
+        db.session.commit()
+        return jsonify({'message': 'Thread deleted successfully'}, 204)
 
-            response_body = {
-                "error": "Customer not found"
-            }
-            status = 404
-        
-        else:
-            
-            db.session.delete(customer)
-            db.session.commit()
+api.add_resource(ThreadsByUniversity, "/universities/<string:schoolname>/threads")
 
-            response_body = {}
-            status = 204
-
-        return make_response(jsonify(response_body), status)
-
-api.add_resource(CustomerById, '/customers/<int:id>')
-
-class Reviews(Resource):
-
+class Posts(Resource):
     def get(self):
-        reviews = Review.query.all()
-
-        response_body = []
-
-        for review in reviews:
-            response_body.append(review.to_dict())
-
-        return make_response(jsonify(response_body), 200)
+        posts = [post.to_dict() for post in Post.query.all()]
+        return make_response(jsonify(posts), 200)
     
     def post(self):
-        json_data = request.get_json()
-        new_review = Review(hotel_id=json_data.get('hotel_id'), customer_id=json_data.get('customer_id'), rating=json_data.get('rating'))
-        db.session.add(new_review)
+        data = request.get_json()
+        user = User.query.filter_by(id=data['post_user_id']).first().to_dict()
+
+        new_post = Post(
+            post_user_id=user['id'], 
+            post_thread_id=data['post_thread_id'], 
+            post_content=data['post_content']
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return jsonify({'message': 'Post created!'}, 201)
+    
+api.add_resource(Posts, "/posts")
+
+class PostByUniThread(Resource):
+    def get(self, schoolname, threadId):
+        thread = Thread.query.filter_by(id=threadId).first()
+
+        if not thread or thread.university.university_name != schoolname:
+            return jsonify({'message': 'Thread not found'}), 404
+
+        posts = [post.to_dict() for post in thread.posts]
+
+        return make_response(jsonify(posts), 200)
+
+    def post(self, schoolname, threadId):
+        thread = Thread.query.filter_by(id=threadId).first()
+
+        if not thread or thread.university.university_name != schoolname:
+            return jsonify({'message': 'Thread not found'}), 404
+
+        data = request.get_json()
+
+        new_post = Post(
+            post_content=data['post_content'],
+            post_thread_id=thread.id
+        )
+
+        db.session.add(new_post)
         db.session.commit()
 
-        response_body = new_review.to_dict()
-        
-        return make_response(jsonify(response_body), 201)
-    
-api.add_resource(Reviews, '/reviews')
+        return jsonify({'message': 'Post created!'}, 201)
 
-class ReviewById(Resource):
-
-    def get(self, id):
-        review = Review.query.filter(Review.id == id).first()
-
-        if not review:
-            response_body = {
-                "error": "Review not found"
-            }
-            status = 404
-        else:
-            response_body = review.to_dict()
-            status = 200
-
-        return make_response(jsonify(response_body), status)
-    
     def patch(self, id):
-        review = Review.query.filter(Review.id == id).first()
+        post = Post.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr(post, attr, data[attr])
+        db.session.commit()
+        response_body = post.to_dict()
+        return make_response(jsonify(response_body), 202)
 
-        if not review:
-            response_body = {
-                "error": "Review not found"
-            }
-            status = 404
-        else:
-            json_data = request.get_json()
-
-            for key in json_data:
-                setattr(review, key, json_data.get(key))
-
-            db.session.commit()
-
-            response_body = review.to_dict()
-            status = 200
-
-        return make_response(jsonify(response_body), status)
-    
     def delete(self, id):
-        review = Review.query.filter(Review.id == id).first()
-        
-        if not review:
+        post = Post.query.filter_by(id=id).first()
+        if not post:
+            return jsonify({'message': 'Post not found'}), 404
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({'message': 'Post deleted successfully'})
 
-            response_body = {
-                "error": "Review not found"
-            }
-            status = 404
-        
-        else:
-            
-            db.session.delete(review)
-            db.session.commit()
+api.add_resource(PostByUniThread, "/universities/<string:schoolname>/threads/<int:threadId>/posts")
 
-            response_body = {}
-            status = 204
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(
+            username=data['username'], 
+            password=data['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'User signed up successfully'}, 201)
+    
+api.add_resource(Signup, "/signup")
 
-        return make_response(jsonify(response_body), status)
-
-api.add_resource(ReviewById, '/reviews/<int:id>')
+# MAKE CHECK-SESSION / LOGIN STUFF
 
 if __name__ == '__main__':
-    app.run(port=7000, debug=True)
+    app.run(port=7777, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,32 +1,109 @@
 #!/usr/bin/env python3
 
 from app import app
-from models import db, Hotel, Customer, Review
+from models import db, University, Thread, User, Post
 
 with app.app_context():
     
-    Hotel.query.delete()
-    Customer.query.delete()
-    Review.query.delete()
+    db.drop_all()
+    db.create_all()
 
-    hotels = []
-    hotels.append(Hotel(name="Marriott", image="https://www.travelandleisure.com/thmb/D-J3iY0h_IBxkZmTQldWUXAuHQg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/renaissance-new-york-midtown-hotel-NYCHOTELS0420-051ef9d668174c978edbb1ee8f6b93e4.jpg"))
-    hotels.append(Hotel(name="Holiday Inn", image="https://www.ihgplc.com/en/-/media/newsrelease_placeholder/holiday-inn-johor-bahru-city-centre---deluxe-king.jpg"))
-    hotels.append(Hotel(name="Hampton Inn", image="https://images.trvl-media.com/lodging/11000000/10970000/10961600/10961545/5b998347.jpg"))
+    university_data = [
+        {"university_name": "Harvard University", "university_color": "rgb(153, 0, 0)"},
+        {"university_name": "Stanford University", "university_color": "rgb(170, 0, 0)"},
+        {"university_name": "Massachusetts Institute of Technology (MIT)", "university_color": "rgb(163, 31, 52)"},
+        {"university_name": "California Institute of Technology (Caltech)", "university_color": "rgb(139, 0, 0)"},
+        {"university_name": "University of Chicago", "university_color": "rgb(131, 39, 41)"},
+        {"university_name": "Princeton University", "university_color": "rgb(0, 47, 108)"},
+        {"university_name": "Yale University", "university_color": "rgb(0, 32, 91)"},
+        {"university_name": "Columbia University", "university_color": "rgb(185, 12, 48)"},
+        {"university_name": "University of Pennsylvania", "university_color": "rgb(0, 21, 42)"},
+        {"university_name": "University of California, Berkeley", "university_color": "rgb(0, 50, 98)"},
+        {"university_name": "University of Michigan - Ann Arbor", "university_color": "rgb(0, 39, 76)"},
+        {"university_name": "Johns Hopkins University", "university_color": "rgb(9, 46, 77)"},
+        {"university_name": "University of California, Los Angeles (UCLA)", "university_color": "rgb(255, 255, 0)"},
+        {"university_name": "Northwestern University", "university_color": "rgb(51, 0, 111)"},
+        {"university_name": "New York University (NYU)", "university_color": "rgb(19, 34, 122)"},
+        {"university_name": "Duke University", "university_color": "rgb(0, 26, 65)"},
+        {"university_name": "University of California, San Diego (UCSD)", "university_color": "rgb(0, 59, 111)"},
+        {"university_name": "University of Wisconsin - Madison", "university_color": "rgb(182, 0, 31)"},
+        {"university_name": "University of Illinois at Urbana-Champaign", "university_color": "rgb(0, 13, 56)"},
+        {"university_name": "University of Texas at Austin", "university_color": "rgb(188, 31, 63)"},
+        {"university_name": "University of Washington", "university_color": "rgb(102, 204, 255)"},
+        {"university_name": "Cornell University", "university_color": "rgb(31, 60, 114)"},
+        {"university_name": "University of California, San Francisco (UCSF)", "university_color": "rgb(0, 75, 105)"},
+        {"university_name": "University of North Carolina at Chapel Hill", "university_color": "rgb(134, 0, 56)"},
+        {"university_name": "University of California, Davis (UC Davis)", "university_color": "rgb(0, 50, 98)"},
+        {"university_name": "University of California, Santa Barbara (UCSB)", "university_color": "rgb(0, 82, 165)"},
+        {"university_name": "University of California, Irvine (UCI)", "university_color": "rgb(0, 118, 183)"},
+        {"university_name": "University of Pittsburgh", "university_color": "rgb(0, 41, 96)"},
+        {"university_name": "University of Southern California (USC)", "university_color": "rgb(153, 0, 0)"},
+        {"university_name": "University of Minnesota - Twin Cities", "university_color": "rgb(125, 0, 32)"},
+        {"university_name": "University of Colorado Boulder", "university_color": "rgb(105, 155, 196)"},
+        {"university_name": "University of Virginia", "university_color": "rgb(0, 33, 71)"},
+        {"university_name": "University of Maryland, College Park", "university_color": "rgb(0, 60, 113)"},
+        {"university_name": "Rice University", "university_color": "rgb(0, 51, 102)"},
+        {"university_name": "Vanderbilt University", "university_color": "rgb(123, 36, 69)"},
+        {"university_name": "Emory University", "university_color": "rgb(0, 82, 147)"},
+        {"university_name": "Washington University in St. Louis", "university_color": "rgb(124, 30, 64)"},
+        {"university_name": "University of Rochester", "university_color": "rgb(10, 68, 121)"},
+        {"university_name": "Boston University", "university_color": "rgb(0, 32, 91)"},
+        {"university_name": "University of Notre Dame", "university_color": "rgb(0, 0, 0)"},
+        {"university_name": "Georgetown University", "university_color": "rgb(0, 24, 66)"},
+        {"university_name": "Carnegie Mellon University", "university_color": "rgb(0, 68, 138)"},
+        {"university_name": "Brown University", "university_color": "rgb(84, 40, 45)"},
+        {"university_name": "Tufts University", "university_color": "rgb(67, 25, 55)"},
+        {"university_name": "University of California, Santa Cruz (UCSC)", "university_color": "rgb(0, 102, 85)"},
+        {"university_name": "University of Florida", "university_color": "rgb(0, 51, 102)"},
+        {"university_name": "University of California, Riverside (UCR)", "university_color": "rgb(0, 68, 127)"},
+        {"university_name": "University of California, San Francisco (UCSF)", "university_color": "rgb(0, 75, 105)"}
+    ]
 
-    customers = []
-    customers.append(Customer(first_name="Alice", last_name="Baker"))
-    customers.append(Customer(first_name="Barry", last_name="Smith"))
-    customers.append(Customer(first_name="Chris", last_name="Jones"))
+    def seed_universities():
+        for data in university_data:
+            university = University(**data)
+            db.session.add(university)
 
-    reviews = []
-    reviews.append(Review(hotel_id=1, customer_id=1, rating=5))
-    reviews.append(Review(hotel_id=2, customer_id=1, rating=5))
-    reviews.append(Review(hotel_id=1, customer_id=2, rating=4))
-    reviews.append(Review(hotel_id=1, customer_id=1, rating=3))
+        db.session.commit()
 
-    db.session.add_all(hotels)
-    db.session.add_all(customers)
-    db.session.add_all(reviews)
-    db.session.commit()
+    
+    thread_data = [
+        {"thread_vote_count": 5, "thread_university_id": 1, "thread_title": "sample"},
+    ]
+    def seed_threads():
+        for data in thread_data:
+            thread = Thread(**data)
+            db.session.add(thread)
+
+        db.session.commit()
+
+    
+    user_data = [
+        {"username": "user1", "password": "password1", "user_university_id": 1},
+    ]
+    def seed_users():
+        for data in user_data:
+            user = User(**data)
+            db.session.add(user)
+
+        db.session.commit()
+
+    
+    post_data = [
+        {"post_content": "Sample post 1", "post_vote_count": 2, "post_user_id": 1, "post_thread_id": 1},
+    ]
+    def seed_posts():
+        for data in post_data:
+            post = Post(**data)
+            db.session.add(post)
+
+        db.session.commit()
+
+    def seed():
+        seed_universities()
+        seed_threads()
+        seed_users()
+        seed_posts()
+    
+    seed()
     print("🌱 Hotels, Customers, and Reviews successfully seeded! 🌱")
